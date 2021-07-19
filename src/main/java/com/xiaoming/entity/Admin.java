f11991460cfd0464d188ed1fe186693c6173d4bb
33028348_zj <15305711706@189.cn>
2021-07-19 13:47:33 +0800

    第一版提交02

---
+++ src/main/java/com/xiaoming/entity/Admin.java
@@ -0,0 +1,50 @@
+package com.xiaoming.entity;
+
+public class Admin {
+    private Integer id;
+    private String name;
+    private String userName;
+    private String pwd;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    @Override
+    public String toString() {
+        return "Admin{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", userName='" + userName + '\'' +
+                ", pwd='" + pwd + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/xiaoming/entity/Student.java
@@ -0,0 +1,11 @@
+package com.xiaoming.entity;
+
+/**
+ * @program: sct
+ * @description: 学生表
+ * @author: qinmf
+ * @create: 2021-07-19 13:45
+ **/
+public class Student {
+
+}
